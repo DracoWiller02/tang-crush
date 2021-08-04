25b2f0b790f6b370e62ed6e1e00820df6b1848a0
DracoWiller02 <88446266+DracoWiller02@users.noreply.github.com>
2021-08-04 21:58:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Làm crs anh nhé hic ?😘😘😘😘😘😘😘😘😘😘',
+    messDesc: 'Làm crs anh nhé hic ?😘😘.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
